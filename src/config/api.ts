92e7af5a7c5b26dc6d19b8fc176405a6a3a6803c
raelyntechnologies <raelyntechnologies@gmail.com>
2026-03-29 01:21:56 +0530

    API

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,5 @@
-const API_URL = "http://localhost:9001/api/v1";
+// const API_URL = "http://localhost:9001/api/v1";
+const API_URL = "http://mono-service-production-22c1.up.railway.app/api/v1";
+
 
 export default API_URL;
