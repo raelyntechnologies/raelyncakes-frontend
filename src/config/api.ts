0028a11507bbbe0ad18e5ea78a925c1a106dc75e
raelyntechnologies <raelyntechnologies@gmail.com>
2026-03-29 14:18:23 +0530

    Bug Fix = Https

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 // const API_URL = "http://localhost:9001/api/v1";
-const API_URL = "http://raelyncakes-backend-production.up.railway.app/api/v1";
+const API_URL = "https://raelyncakes-backend-production.up.railway.app/api/v1";
 
 
 export default API_URL;
