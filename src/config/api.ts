dc758839d7eac44d39b0ee74c7f0ecb4e1efc459
raelyntechnologies <raelyntechnologies@gmail.com>
2026-04-29 20:57:35 +0530

    API URL

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-// const API_URL = "http://localhost:9001/api/v1";
+//const API_URL = "http://localhost:9001/api/v1";
 const API_URL = "https://raelyncakes-backend-production.up.railway.app/api/v1";
 
 
